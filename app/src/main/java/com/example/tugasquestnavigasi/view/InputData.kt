0558363dc20c0cc8a3468435da6ccd41f6da3458
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-08 19:22:05 +0700

    Membuat halaman ketiga dengan nama inputdata dan membuat fungsi form input serta membuat 2 event untuk navigasi dan membuat bebrapa variabel var dan val

---
+++ app/src/main/java/com/example/tugasquestnavigasi/view/InputData.kt
@@ -0,0 +1,70 @@
+package com.example.tugasquestnavigasi.view
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.RadioButtonDefaults
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugasquestnavigasi.R
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    OnSubmitBtnClick: () -> Unit,
+    OnBackBtnClick: () -> Unit = {}
+) {
+    var textNama by remember { mutableStateOf("") }
+    var textJenis by remember { mutableStateOf("") }
+    var textTipeFans by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+
+    var Nama by remember { mutableStateOf("") }
+    var JenisKelamin by remember { mutableStateOf("") }
+    var Fans by remember { mutableStateOf("") }
+    var Alamat by remember { mutableStateOf("") }
+
+    val Jenis = listOf("Laki-Laki", "Perempuan")
+    val TipeFans = listOf("Fans Biasa", "Fans Aktif", "Ultras")
+
+
+
+    }
+
+
+
+
