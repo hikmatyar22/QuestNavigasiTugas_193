2cd7302bddec7c6db09ebccbf290ea8a9a44b68d
hikmatyar22 <hikmatyaralghifary421@gmail.com>
2025-11-08 19:02:38 +0700

    Membuat halaman pertma dengan nama tampilanhome dan membuat fungsi tampil beranda serta event onnavigatetoform untuk navigasi

---
+++ app/src/main/java/com/example/tugasquestnavigasi/view/TampilanHome.kt
@@ -0,0 +1,44 @@
+package com.example.tugasquestnavigasi.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tugasquestnavigasi.R
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilanBeranda(
+    onNavigateToForm: () -> Unit
+) {
+
+}
